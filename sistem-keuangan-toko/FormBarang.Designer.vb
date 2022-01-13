faa81aa63696c1a344e993e8eac7ad4d50fa6b25
BintangIjlal <bintang.ijlal@gmail.com>
2022-01-13 14:05:45 +0700

    Tinggal Jenis Barang dan Pembuatan Class Biar rapih

--- sistem-keuangan-toko/FormBarang.Designer.vb
+++ sistem-keuangan-toko/FormBarang.Designer.vb
@@ -43,6 +43,7 @@ Partial Class FormBarang
         Me.PilihFormLainnyaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.TSMenu_FormJenisBarang = New System.Windows.Forms.ToolStripMenuItem()
         Me.TSMenu_FormPenjualan = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TSMenu_FormBarangMasuk = New System.Windows.Forms.ToolStripMenuItem()
         Me.Panel1 = New System.Windows.Forms.Panel()
         CType(Me.DGV_Barang, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.MenuStrip1.SuspendLayout()
@@ -55,126 +56,145 @@ Partial Class FormBarang
         '
         'DGV_Barang
         '
+        Me.DGV_Barang.BackgroundColor = System.Drawing.Color.DarkSlateGray
         Me.DGV_Barang.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DGV_Barang.Location = New System.Drawing.Point(12, 396)
+        Me.DGV_Barang.Location = New System.Drawing.Point(14, 396)
         Me.DGV_Barang.Name = "DGV_Barang"
         Me.DGV_Barang.RowHeadersWidth = 51
         Me.DGV_Barang.RowTemplate.Height = 29
-        Me.DGV_Barang.Size = New System.Drawing.Size(829, 325)
+        Me.DGV_Barang.Size = New System.Drawing.Size(933, 325)
         Me.DGV_Barang.TabIndex = 5
         '
         'Txt_NamaBarang
         '
-        Me.Txt_NamaBarang.Location = New System.Drawing.Point(168, 30)
+        Me.Txt_NamaBarang.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Txt_NamaBarang.Location = New System.Drawing.Point(189, 30)
         Me.Txt_NamaBarang.Name = "Txt_NamaBarang"
-        Me.Txt_NamaBarang.Size = New System.Drawing.Size(250, 27)
+        Me.Txt_NamaBarang.Size = New System.Drawing.Size(281, 27)
         Me.Txt_NamaBarang.TabIndex = 7
         '
         'Txt_stock
         '
-        Me.Txt_stock.Location = New System.Drawing.Point(168, 75)
+        Me.Txt_stock.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Txt_stock.Location = New System.Drawing.Point(189, 75)
         Me.Txt_stock.Name = "Txt_stock"
-        Me.Txt_stock.Size = New System.Drawing.Size(250, 27)
+        Me.Txt_stock.Size = New System.Drawing.Size(281, 27)
         Me.Txt_stock.TabIndex = 8
         '
         'Txt_harga
         '
-        Me.Txt_harga.Location = New System.Drawing.Point(168, 118)
+        Me.Txt_harga.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Txt_harga.Location = New System.Drawing.Point(189, 118)
         Me.Txt_harga.Name = "Txt_harga"
-        Me.Txt_harga.Size = New System.Drawing.Size(250, 27)
+        Me.Txt_harga.Size = New System.Drawing.Size(281, 27)
         Me.Txt_harga.TabIndex = 9
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(14, 33)
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(16, 33)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(100, 20)
+        Me.Label2.Size = New System.Drawing.Size(109, 20)
         Me.Label2.TabIndex = 11
         Me.Label2.Text = "Nama Barang"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(14, 75)
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(16, 75)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(45, 20)
+        Me.Label3.Size = New System.Drawing.Size(47, 20)
         Me.Label3.TabIndex = 12
         Me.Label3.Text = "Stock"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(14, 121)
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.Label4.Location = New System.Drawing.Point(16, 121)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(50, 20)
+        Me.Label4.Size = New System.Drawing.Size(53, 20)
         Me.Label4.TabIndex = 13
         Me.Label4.Text = "Harga"
         '
         'Date_Masuk
         '
-        Me.Date_Masuk.Location = New System.Drawing.Point(168, 163)
+        Me.Date_Masuk.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Date_Masuk.Location = New System.Drawing.Point(189, 163)
         Me.Date_Masuk.Name = "Date_Masuk"
-        Me.Date_Masuk.Size = New System.Drawing.Size(250, 27)
+        Me.Date_Masuk.Size = New System.Drawing.Size(281, 27)
         Me.Date_Masuk.TabIndex = 14
         '
         'Date_Kadaluarsa
         '
-        Me.Date_Kadaluarsa.Location = New System.Drawing.Point(168, 211)
+        Me.Date_Kadaluarsa.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Date_Kadaluarsa.Location = New System.Drawing.Point(189, 211)
         Me.Date_Kadaluarsa.Name = "Date_Kadaluarsa"
-        Me.Date_Kadaluarsa.Size = New System.Drawing.Size(250, 27)
+        Me.Date_Kadaluarsa.Size = New System.Drawing.Size(281, 27)
         Me.Date_Kadaluarsa.TabIndex = 15
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(14, 168)
+        Me.Label5.Font = New System.Drawing.Font("Segoe UI", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.Label5.Location = New System.Drawing.Point(16, 168)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(107, 20)
+        Me.Label5.Size = New System.Drawing.Size(118, 20)
         Me.Label5.TabIndex = 16
         Me.Label5.Text = "Tanggal Masuk"
         '
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(14, 215)
+        Me.Label6.Font = New System.Drawing.Font("Segoe UI", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.Label6.Location = New System.Drawing.Point(16, 215)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(138, 20)
+        Me.Label6.Size = New System.Drawing.Size(154, 20)
         Me.Label6.TabIndex = 17
         Me.Label6.Text = "Tanggal Kadaluarsa"
         '
         'Btn_TambahBarang
         '
-        Me.Btn_TambahBarang.Location = New System.Drawing.Point(508, 97)
+        Me.Btn_TambahBarang.BackColor = System.Drawing.Color.Teal
+        Me.Btn_TambahBarang.ForeColor = System.Drawing.SystemColors.Control
+        Me.Btn_TambahBarang.Location = New System.Drawing.Point(572, 103)
         Me.Btn_TambahBarang.Name = "Btn_TambahBarang"
-        Me.Btn_TambahBarang.Size = New System.Drawing.Size(299, 29)
+        Me.Btn_TambahBarang.Size = New System.Drawing.Size(336, 29)
         Me.Btn_TambahBarang.TabIndex = 18
         Me.Btn_TambahBarang.Text = "Tambah Barang"
-        Me.Btn_TambahBarang.UseVisualStyleBackColor = True
+        Me.Btn_TambahBarang.UseVisualStyleBackColor = False
         '
         'Btn_UpdateBarang
         '
-        Me.Btn_UpdateBarang.Location = New System.Drawing.Point(508, 145)
+        Me.Btn_UpdateBarang.BackColor = System.Drawing.Color.Teal
+        Me.Btn_UpdateBarang.ForeColor = System.Drawing.SystemColors.Control
+        Me.Btn_UpdateBarang.Location = New System.Drawing.Point(572, 145)
         Me.Btn_UpdateBarang.Name = "Btn_UpdateBarang"
-        Me.Btn_UpdateBarang.Size = New System.Drawing.Size(299, 29)
+        Me.Btn_UpdateBarang.Size = New System.Drawing.Size(336, 29)
         Me.Btn_UpdateBarang.TabIndex = 19
         Me.Btn_UpdateBarang.Text = "Update Barang"
-        Me.Btn_UpdateBarang.UseVisualStyleBackColor = True
+        Me.Btn_UpdateBarang.UseVisualStyleBackColor = False
         '
         'Btn_HapusBarang
         '
-        Me.Btn_HapusBarang.Location = New System.Drawing.Point(508, 197)
+        Me.Btn_HapusBarang.BackColor = System.Drawing.Color.Teal
+        Me.Btn_HapusBarang.ForeColor = System.Drawing.SystemColors.Control
+        Me.Btn_HapusBarang.Location = New System.Drawing.Point(572, 188)
         Me.Btn_HapusBarang.Name = "Btn_HapusBarang"
-        Me.Btn_HapusBarang.Size = New System.Drawing.Size(299, 29)
+        Me.Btn_HapusBarang.Size = New System.Drawing.Size(336, 29)
         Me.Btn_HapusBarang.TabIndex = 20
         Me.Btn_HapusBarang.Text = "Hapus Barang"
-        Me.Btn_HapusBarang.UseVisualStyleBackColor = True
+        Me.Btn_HapusBarang.UseVisualStyleBackColor = False
         '
         'Label7
         '
         Me.Label7.AutoSize = True
+        Me.Label7.BackColor = System.Drawing.Color.DarkCyan
         Me.Label7.Font = New System.Drawing.Font("Segoe UI", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Label7.Location = New System.Drawing.Point(24, 52)
+        Me.Label7.ForeColor = System.Drawing.SystemColors.Control
+        Me.Label7.Location = New System.Drawing.Point(27, 52)
         Me.Label7.Name = "Label7"
         Me.Label7.Size = New System.Drawing.Size(152, 31)
         Me.Label7.TabIndex = 21
@@ -184,7 +204,7 @@ Partial Class FormBarang
         '
         Me.Label8.AutoSize = True
         Me.Label8.Font = New System.Drawing.Font("Segoe UI Semibold", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
-        Me.Label8.Location = New System.Drawing.Point(334, 356)
+        Me.Label8.Location = New System.Drawing.Point(376, 356)
         Me.Label8.Name = "Label8"
         Me.Label8.Size = New System.Drawing.Size(166, 28)
         Me.Label8.TabIndex = 22
@@ -192,18 +212,20 @@ Partial Class FormBarang
         '
         'MenuStrip1
         '
+        Me.MenuStrip1.BackColor = System.Drawing.Color.DarkCyan
         Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
         Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PilihFormLainnyaToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(853, 29)
+        Me.MenuStrip1.Padding = New System.Windows.Forms.Padding(7, 2, 0, 2)
+        Me.MenuStrip1.Size = New System.Drawing.Size(960, 29)
         Me.MenuStrip1.TabIndex = 24
         Me.MenuStrip1.Text = "MenuStrip1"
         '
         'PilihFormLainnyaToolStripMenuItem
         '
-        Me.PilihFormLainnyaToolStripMenuItem.BackColor = System.Drawing.SystemColors.ControlLight
-        Me.PilihFormLainnyaToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.TSMenu_FormJenisBarang, Me.TSMenu_FormPenjualan})
+        Me.PilihFormLainnyaToolStripMenuItem.BackColor = System.Drawing.Color.LightCyan
+        Me.PilihFormLainnyaToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.TSMenu_FormJenisBarang, Me.TSMenu_FormPenjualan, Me.TSMenu_FormBarangMasuk})
         Me.PilihFormLainnyaToolStripMenuItem.Font = New System.Drawing.Font("Myriad Pro Light", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
         Me.PilihFormLainnyaToolStripMenuItem.Name = "PilihFormLainnyaToolStripMenuItem"
         Me.PilihFormLainnyaToolStripMenuItem.Size = New System.Drawing.Size(162, 25)
@@ -212,17 +234,24 @@ Partial Class FormBarang
         'TSMenu_FormJenisBarang
         '
         Me.TSMenu_FormJenisBarang.Name = "TSMenu_FormJenisBarang"
-        Me.TSMenu_FormJenisBarang.Size = New System.Drawing.Size(232, 26)
+        Me.TSMenu_FormJenisBarang.Size = New System.Drawing.Size(244, 26)
         Me.TSMenu_FormJenisBarang.Text = "Form Jenis Barang"
         '
         'TSMenu_FormPenjualan
         '
         Me.TSMenu_FormPenjualan.Name = "TSMenu_FormPenjualan"
-        Me.TSMenu_FormPenjualan.Size = New System.Drawing.Size(232, 26)
+        Me.TSMenu_FormPenjualan.Size = New System.Drawing.Size(244, 26)
         Me.TSMenu_FormPenjualan.Text = "Form Penjualan"
+        '
+        'TSMenu_FormBarangMasuk
+        '
+        Me.TSMenu_FormBarangMasuk.Name = "TSMenu_FormBarangMasuk"
+        Me.TSMenu_FormBarangMasuk.Size = New System.Drawing.Size(244, 26)
+        Me.TSMenu_FormBarangMasuk.Text = "Form Barang Masuk"
         '
         'Panel1
         '
+        Me.Panel1.BackColor = System.Drawing.Color.Turquoise
         Me.Panel1.Controls.Add(Me.Label2)
         Me.Panel1.Controls.Add(Me.Label4)
         Me.Panel1.Controls.Add(Me.Label5)
@@ -233,16 +262,17 @@ Partial Class FormBarang
         Me.Panel1.Controls.Add(Me.Date_Kadaluarsa)
         Me.Panel1.Controls.Add(Me.Txt_NamaBarang)
         Me.Panel1.Controls.Add(Me.Txt_stock)
-        Me.Panel1.Location = New System.Drawing.Point(12, 70)
+        Me.Panel1.Location = New System.Drawing.Point(14, 70)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(446, 267)
+        Me.Panel1.Size = New System.Drawing.Size(502, 267)
         Me.Panel1.TabIndex = 25
         '
         'FormBarang
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(853, 733)
+        Me.BackColor = System.Drawing.Color.PaleTurquoise
+        Me.ClientSize = New System.Drawing.Size(960, 733)
         Me.Controls.Add(Me.Label8)
         Me.Controls.Add(Me.Label7)
         Me.Controls.Add(Me.Btn_HapusBarang)
@@ -251,6 +281,7 @@ Partial Class FormBarang
         Me.Controls.Add(Me.DGV_Barang)
         Me.Controls.Add(Me.MenuStrip1)
         Me.Controls.Add(Me.Panel1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
         Me.MainMenuStrip = Me.MenuStrip1
         Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Name = "FormBarang"
@@ -286,4 +317,5 @@ Partial Class FormBarang
     Friend WithEvents TSMenu_FormJenisBarang As ToolStripMenuItem
     Friend WithEvents TSMenu_FormPenjualan As ToolStripMenuItem
     Friend WithEvents Panel1 As Panel
+    Friend WithEvents TSMenu_FormBarangMasuk As ToolStripMenuItem
 End Class
--- sistem-keuangan-toko/FormBarang.vb
+++ sistem-keuangan-toko/FormBarang.vb
@@ -20,16 +20,29 @@ Public Class FormBarang
         ' Add any initialization after the InitializeComponent() call.
         Barang = New ClassBarang()
         ResultDGV()
-        Txt_NamaBarang.Text = Barang.GSNamaBarangProperty
-        Txt_stock.Text = Barang.GSStockProperty
-        Txt_harga.Text = Barang.GShargaBarangProperty
+        'Txt_NamaBarang.Text = Barang.GSNamaBarangProperty
+        'Txt_stock.Text = Barang.GSStockProperty
+        'Txt_harga.Text = Barang.GShargaBarangProperty
         'Date_Masuk.Format = DateTimePickerFormat.Custom
         'Date_Masuk.CustomFormat = "yyyy/mm/dd"
         'Date_Masuk.Value = Barang.GStanggalMasukBarangProperty
         'Date_Kadaluarsa.Value = Barang.GStanggalkadaluarsaBarangProperty
+        'Date_Kadaluarsa.CustomFormat = "yyyy/mm/dd"
+        'Date_Kadaluarsa.Value = Barang.GStanggalkadaluarsaBarangProperty
 
 
     End Sub
+
+    'Sub NoticeInput()
+    '    Dim resultInput As DialogResult = MessageBox.Show("message", "caption", MessageBoxButtons.YesNoCancel)
+    '    If resultInput = DialogResult.Cancel Then
+    '        MessageBox.Show("Cancel pressed")
+    '    ElseIf resultInput = DialogResult.No Then
+    '        MessageBox.Show("No pressed")
+    '    ElseIf resultInput = DialogResult.Yes Then
+    '        MessageBox.Show("Yes pressed")
+    '    End If
+    'End Sub
 
     Sub ResultDGV()
         mycmd.Connection = myconnection.open
@@ -57,8 +70,8 @@ Public Class FormBarang
                                 tanggal_masuk, tanggal_kadaluarsa)
                                 VALUES('" _
                                 & Txt_NamaBarang.Text & "', '" _
-                                & Txt_stock.Text & "', '" _
-                                & Txt_harga.Text & "', '" _
+                                & Txt_stock.Text & "+ jumlah_barang_masuk.barang_masuk" & "', '" _
+                                & Txt_harga.Text.ToString & "', '" _
                                 & Date_Masuk.Value.ToString("yyyy/MM/dd") & "', '" _
                                 & Date_Kadaluarsa.Value.ToString("yyyy/MM/dd") & "')"
         mycmd.ExecuteNonQuery()
@@ -67,41 +80,102 @@ Public Class FormBarang
         MsgBox("Data tersimpan !", MsgBoxStyle.Information, "Pemberitahuan")
     End Sub
 
+    'Sub GetJumlahBarangMasuk()
+    '    mycmd.Connection = myconnection.open
+    '    mycmd.CommandText = "+(SELECT jumlah_barang_masuk FROM barang_masuk WHERE barang_masuk.id_barang_masuk=" & id_barang & ")"
+    'End Sub
+
+
     Private Sub Btn_UpdateBarang_Click(sender As Object, e As EventArgs) Handles Btn_UpdateBarang.Click
-        'Dim selectedBarang As List(Of String) = Barang.GetDataKoleksiByIDDatabase(selectedTableBarang)
+        If Txt_NamaBarang.Text = "" Or Txt_stock.Text = "" Or Txt_harga.Text = "" Then
+            MessageBox.Show("Pastikan semua Box terisi Datanya")
+        ElseIf Date_Kadaluarsa.Value < Date_Masuk.Value Then
+            MessageBox.Show("Pastikan Tanggal sudah benar")
+        Else
+            Txt_harga.Text = FormatNumber(Txt_harga.Text, 2, TriState.True)
+            Dim resultInput As DialogResult = MessageBox.Show("Apakah Input sudah benar?", "Pemberitahuan", MessageBoxButtons.YesNo)
 
-        ''Dim ID As Integer = selectedBarang(0)
-        'Barang.GSNamaBarangProperty = selectedBarang(1)
-        'Barang.GSStockProperty = selectedBarang(2)
-        'Barang.GShargaBarangProperty = selectedBarang(3)
-        'Barang.GStanggalMasukBarangProperty = selectedBarang(4)
-        'Barang.GStanggalkadaluarsaBarangProperty = selectedBarang(5)
-
-        mycmd.Connection = myconnection.open
-        mycmd.CommandText = "UPDATE barang SET " &
-                        “nama_barang='" & Txt_NamaBarang.Text & "', " &
-                        “stock='" & Txt_stock.Text & "', " &
-                        “harga='" & Txt_harga.Text & "', " &
+            If resultInput = DialogResult.No Then
+                Txt_harga.Text = Txt_harga.Text.Replace(".", String.Empty)
+                MessageBox.Show("Update dibatalkan")
+            ElseIf resultInput = DialogResult.Yes Then
+                Txt_harga.Text = Txt_harga.Text.Replace(".", String.Empty)
+                mycmd.Connection = myconnection.open
+                mycmd.CommandText = "UPDATE barang " &
+                        “SET nama_barang='" & Txt_NamaBarang.Text & "', " &
+                        "stock= IFNULL((SELECT SUM(jumlah_barang_masuk) FROM barang_masuk WHERE id_barang='" & id_barang & "'),0)+'" & Txt_stock.Text & "', " &
+                        "harga= IFNULL('" & Txt_harga.Text & "',0), " &
                         “tanggal_masuk='" & Date_Masuk.Value.ToString("yyyy/MM/dd") & "', " &
                         “tanggal_kadaluarsa='" & Date_Kadaluarsa.Value.ToString("yyyy/MM/dd") & "' " &
-                        "WHERE id_barang='" & id_barang & "' "
-        mycmd.ExecuteNonQuery()
-        myconnection.close()
+                        "WHERE id_barang ='" & id_barang & "' "
+                mycmd.ExecuteNonQuery()
+                myconnection.close()
+                ResultDGV()
+                MsgBox("Data terupdate !", MsgBoxStyle.Information, "Pemberitahuan")
+            End If
+        End If
 
-        'Barang.UpdateDataBarangByIODatabase(id_barang, Txt_NamaBarang.Text, Txt_stock.Text, Txt_harga.Text, Date_Masuk.Value.ToString("yyyy/MM/dd"), Date_Kadaluarsa.Value.ToString("yyyy/MM/dd"))
-        ResultDGV()
-        MsgBox("Data terupdate !", MsgBoxStyle.Information, "Pemberitahuan")
     End Sub
 
+    'Update satu query saja
+    '"UPDATE barang " &
+    '        “SET nama_barang='" & Txt_NamaBarang.Text & "', " &
+    '        "stock= IFNULL((SELECT jumlah_barang_masuk FROM barang_masuk WHERE id_barang='" & id_barang & "'),0)+'" & Txt_stock.Text & "', " &
+    '        "harga='" & Txt_harga.Text & "', " &
+    '        “tanggal_masuk='" & Date_Masuk.Value.ToString("yyyy/MM/dd") & "', " &
+    '        “tanggal_kadaluarsa='" & Date_Kadaluarsa.Value.ToString("yyyy/MM/dd") & "' " &
+    '        "WHERE id_barang ='" & id_barang & "' "
+
+    'Dim selectedBarang As List(Of String) = Barang.GetDataKoleksiByIDDatabase(selectedTableBarang)
+
+    ''Dim ID As Integer = selectedBarang(0)
+    'Barang.GSNamaBarangProperty = selectedBarang(1)
+    'Barang.GSStockProperty = selectedBarang(2)
+    'Barang.GShargaBarangProperty = selectedBarang(3)
+    'Barang.GStanggalMasukBarangProperty = selectedBarang(4)
+    'Barang.GStanggalkadaluarsaBarangProperty = selectedBarang(5)
+
+
+
+    'Barang.UpdateDataBarangByIODatabase(id_barang, Txt_NamaBarang.Text, Txt_stock.Text, Txt_harga.Text, Date_Masuk.Value.ToString("yyyy/MM/dd"), Date_Kadaluarsa.Value.ToString("yyyy/MM/dd"))
+
+
+    'Public Sub updatedatatable()
+    '    DGV_Barang.Rows.Clear()
+    '    For Each rowkoleksi In Barang.getBarangDataTable()
+    '        Dim datatable As String() = {rowkoleksi(1),
+    '                                    rowkoleksi(2),
+    '                                    rowkoleksi(3),
+    '                                    rowkoleksi(4),
+    '                                    rowkoleksi(5),
+    '                                    rowkoleksi(6),
+    '                                    rowkoleksi(7),
+    '                                    rowkoleksi(8),
+    '                                    rowkoleksi(9)}
+    '        DGV_Barang.Rows.Add(datatable)
+    '    Next
+    'End Sub
+
     Private Sub Btn_HapusBarang_Click(sender As Object, e As EventArgs) Handles Btn_HapusBarang.Click
+        If Txt_NamaBarang.Text = "" Or Txt_stock.Text = "" Or Txt_harga.Text = "" Then
+            MsgBox("Pastikan semua box terisi datanya")
+        Else
+            Dim resultInput As DialogResult = MessageBox.Show("Apakah Input sudah benar?", "Pemberitahuan", MessageBoxButtons.YesNo)
+
+            If resultInput = DialogResult.No Then
+                MessageBox.Show("Hapus dibatalkan")
+            ElseIf resultInput = DialogResult.Yes Then
+                mycmd.Connection = myconnection.open
+                mycmd.CommandText = “DELETE FROM barang “ &
+                           “WHERE id_barang='" & id_barang & "'"
+                mycmd.ExecuteNonQuery()
+                myconnection.close()
+                ResultDGV()
+                MsgBox("Data terhapus !", MsgBoxStyle.Information, "Pemberitahuan")
+            End If
+        End If
         'Dim ID As Integer
-        mycmd.Connection = myconnection.open
-        mycmd.CommandText = “DELETE FROM barang “ &
-                       “WHERE id_barang='" & id_barang & "'"
-        mycmd.ExecuteNonQuery()
-        myconnection.close()
-        ResultDGV()
-        MsgBox("Data terhapus !", MsgBoxStyle.Information, "Pemberitahuan")
+
     End Sub
 
     Private Sub DGV_Barang_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DGV_Barang.CellClick
@@ -128,25 +202,59 @@ Public Class FormBarang
     End Sub
 
     Private Sub TSMenu_FormPenjualan_Click(sender As Object, e As EventArgs) Handles TSMenu_FormPenjualan.Click
-        'Dim formPenjualan = New FormPenjualan()
-        'formPenjualan.Show()
+        Dim formPenjualan = New FormPenjualan()
+        formPenjualan.Show()
     End Sub
 
-    '    Public Sub updatedatatable()
-    '        DGV_Barang.Rows.Clear()
-    '        For Each rowkoleksi In Barang.getBarangDataTable()
-    '            Dim datatable As String() = {rowkoleksi(1),
-    '                                        rowkoleksi(2),
-    '                                        rowkoleksi(3),
-    '                                        rowkoleksi(4),
-    '                                        rowkoleksi(5),
-    '                                        rowkoleksi(6),
-    '                                        rowkoleksi(7),
-    '                                        rowkoleksi(8),
-    '                                        rowkoleksi(9)}
-    '            DGV_Barang.Rows.Add(datatable)
-    '        Next
-    '    End Sub
+    Private Sub FormBarangMasukToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles TSMenu_FormBarangMasuk.Click
+        Dim formBarangMasuk = New FormBarangMasuk()
+        formBarangMasuk.Show()
+    End Sub
+
+    'Private Function updatebarangfrombarangmasuktobarang(id_barang As Integer, jumlah_barang_masuk As Integer)
+    '    mycmd.Connection = myconnection.open
+    '    mycmd.CommandText = "UPDATE barang SET stock="
+    '    mycmd.ExecuteNonQuery()
+    '    myconnection.close()
+    'End Function
+
+    'Private Sub Txt_harga_Leave(sender As Object, e As EventArgs) Handles Txt_harga.Change
+    '    'If Txt_harga.Text = "" Then
+    '    '    Txt_harga.Text = "0"
+    '    'Else
+    '    '    'Txt_harga.Text = Convert.ToDecimal(Txt_harga.Text)
+    '    '    Txt_harga.Text = FormatCurrency(Txt_harga.Text)
+    '    'End If
+    'End Sub
+
+    'Sub FormatAngka()
+    '    If IsNumeric(Txt_harga) Then
+    '        Txt_harga.Text = Format(Txt_harga, "#,##")
+    '    End If
+    'End Sub
+
+    'Private Sub Txt_harga_Leave(sender As Object, e As EventArgs) Handles Txt_harga.Leave
+    '    'Dim hapus As String
+
+    '    'If Txt_harga.Text = "0" Then
+    '    '    Txt_harga.Text = "Rp0"
+    '    'Else
+    '    '    'Txt_harga.Text = Convert.ToDecimal(Txt_harga.Text)
+
+    '    '    Dim subst As String = Txt_harga.Text.Substring(2)
+    '    '    Txt_harga.Text = FormatCurrency(subst)
+    '    '    MessageBox.Show(subst)
+    '    'End If
+
+    '    'Txt_harga.Text = FormatNumber(Txt_harga.Text, 2, TriState.True)
+    '    'MessageBox.Show(Txt_harga.Text)
+
+    '    'If IsNumeric(Txt_harga) Then
+    '    '    Txt_harga.Text = Format(Txt_harga, "#,##0")
+    '    'End If
+    'End Sub
+
+
 
     '    Private Sub dgv_barang_cellclick(sender As Object, e As DataGridViewCellEventArgs) Handles DGV_Barang.CellClick
     '        Dim index As Integer = e.RowIndex
